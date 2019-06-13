410daa7e0431a4cef73a014827737c4618512d48
mooremok <mooremok@163.com>
2019-06-13 17:18:27 +0800

    修改base.py模板配置

--- typeidea/settings/base.py
+++ typeidea/settings/base.py
@@ -63,6 +63,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        #'DIRS': [os.path.join(BASE_DIR, 'themes', THEME, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
